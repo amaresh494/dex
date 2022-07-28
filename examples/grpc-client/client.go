ab48e59c5ab09d8010dc1c156501f2b4b969e7ee
amaresh494 <48903187+amaresh494@users.noreply.github.com>
2022-07-28 17:43:20 +0530

    Update client.go

--- examples/grpc-client/client.go
+++ examples/grpc-client/client.go
@@ -6,11 +6,11 @@ import (
 	"crypto/x509"
 	"flag"
 	"fmt"
-	"log"
-	"os"
-
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+	"log"
+	"os"
+	"strings"
 
 	"github.com/dexidp/dex/api/v2"
 )
@@ -43,96 +43,35 @@ func newDexClient(hostAndPort, caPath, clientCrt, clientKey string) (api.DexClie
 	return api.NewDexClient(conn), nil
 }
 
-func createPassword(cli api.DexClient) error {
-	p := api.Password{
-		Email: "test@example.com",
-		// bcrypt hash of the value "test1" with cost 10
-		Hash:     []byte("$2a$10$XVMN/Fid.Ks4CXgzo8fpR.iU1khOMsP5g9xQeXuBm1wXjRX8pjUtO"),
-		Username: "test",
-		UserId:   "test",
-	}
-
-	createReq := &api.CreatePasswordReq{
-		Password: &p,
-	}
-
-	// Create password.
-	if resp, err := cli.CreatePassword(context.TODO(), createReq); err != nil || resp.AlreadyExists {
-		if resp.AlreadyExists {
-			return fmt.Errorf("Password %s already exists", createReq.Password.Email)
-		}
-		return fmt.Errorf("failed to create password: %v", err)
-	}
-	log.Printf("Created password with email %s", createReq.Password.Email)
-
-	// List all passwords.
-	resp, err := cli.ListPasswords(context.TODO(), &api.ListPasswordReq{})
-	if err != nil {
-		return fmt.Errorf("failed to list password: %v", err)
-	}
-
-	log.Print("Listing Passwords:\n")
-	for _, pass := range resp.Passwords {
-		log.Printf("%+v", pass)
-	}
-
-	// Verifying correct and incorrect passwords
-	log.Print("Verifying Password:\n")
-	verifyReq := &api.VerifyPasswordReq{
-		Email:    "test@example.com",
-		Password: "test1",
-	}
-	verifyResp, err := cli.VerifyPassword(context.TODO(), verifyReq)
-	if err != nil {
-		return fmt.Errorf("failed to run VerifyPassword for correct password: %v", err)
-	}
-	if !verifyResp.Verified {
-		return fmt.Errorf("failed to verify correct password: %v", verifyResp)
-	}
-	log.Printf("properly verified correct password: %t\n", verifyResp.Verified)
-
-	badVerifyReq := &api.VerifyPasswordReq{
-		Email:    "test@example.com",
-		Password: "wrong_password",
-	}
-	badVerifyResp, err := cli.VerifyPassword(context.TODO(), badVerifyReq)
-	if err != nil {
-		return fmt.Errorf("failed to run VerifyPassword for incorrect password: %v", err)
-	}
-	if badVerifyResp.Verified {
-		return fmt.Errorf("verify returned true for incorrect password: %v", badVerifyResp)
-	}
-	log.Printf("properly failed to verify incorrect password: %t\n", badVerifyResp.Verified)
-
-	log.Print("Listing Passwords:\n")
-	for _, pass := range resp.Passwords {
-		log.Printf("%+v", pass)
-	}
-
-	deleteReq := &api.DeletePasswordReq{
-		Email: p.Email,
-	}
-
-	// Delete password with email = test@example.com.
-	if resp, err := cli.DeletePassword(context.TODO(), deleteReq); err != nil || resp.NotFound {
-		if resp.NotFound {
-			return fmt.Errorf("Password %s not found", deleteReq.Email)
-		}
-		return fmt.Errorf("failed to delete password: %v", err)
-	}
-	log.Printf("Deleted password with email %s", deleteReq.Email)
-
-	return nil
-}
-
 func main() {
 	caCrt := flag.String("ca-crt", "", "CA certificate")
 	clientCrt := flag.String("client-crt", "", "Client certificate")
 	clientKey := flag.String("client-key", "", "Client key")
+	clientId := flag.String("client-id", "", "Client ID")
+	clientName := flag.String("client-name", "", "Client Name")
+	clientSecret := flag.String("client-secret", "", "Client Secret")
+	redirectURIs := flag.String("redirect-uris", "", "Redirect URIs")
+	remove := flag.Bool("remove", false, "Remove client")
 	flag.Parse()
 
-	if *clientCrt == "" || *caCrt == "" || *clientKey == "" {
-		log.Fatal("Please provide CA & client certificates and client key. Usage: ./client --ca-crt=<path ca.crt> --client-crt=<path client.crt> --client-key=<path client key>")
+	usage := "(Usage: --ca-crt=<ca-cert-path> --client-crt=<path client.crt> --client-key=<path client key>" +
+		" --client-id=<client-id> --client-name=<client-name> --client-secret=<client-secret> " +
+		"--redirect-uris=<comma separated uris> --delete=<delete flag>)"
+
+	if *caCrt == "" {
+		log.Fatal("Please provide CA certificate. " + usage)
+	}
+
+	if *clientCrt == "" {
+		log.Fatal("Please provide Client certificate. " + usage)
+	}
+
+	if *clientKey == "" {
+		log.Fatal("Please provide Client key. " + usage)
+	}
+
+	if *clientId == "" {
+		log.Fatal("Please provide Client ID. " + usage)
 	}
 
 	client, err := newDexClient("127.0.0.1:5557", *caCrt, *clientCrt, *clientKey)
@@ -140,7 +79,39 @@ func main() {
 		log.Fatalf("failed creating dex client: %v ", err)
 	}
 
-	if err := createPassword(client); err != nil {
-		log.Fatalf("testPassword failed: %v", err)
+	if !*remove {
+		if *clientName == "" {
+			log.Fatal("Please provide Client Name. " + usage)
+		}
+
+		if *clientSecret == "" {
+			log.Fatal("Please provide Client Secret. " + usage)
+		}
+
+		if *redirectURIs == "" {
+			log.Fatal("Please provide Client Secret. " + usage)
+		}
+		redirectUriList := strings.Split(*redirectURIs, ",")
+
+		req := &api.CreateClientReq{
+			Client: &api.Client{
+				Id:           *clientId,
+				Name:         *clientName,
+				Secret:       *clientSecret,
+				RedirectUris: redirectUriList,
+			},
+		}
+
+		if _, err := client.CreateClient(context.TODO(), req); err != nil {
+			log.Fatalf("failed creating oauth2 client: %v", err)
+		}
+	} else {
+		req := &api.DeleteClientReq{
+			Id: *clientId,
+		}
+
+		if _, err := client.DeleteClient(context.TODO(), req); err != nil {
+			log.Fatalf("failed deleting oauth2 client: %v", err)
+		}
 	}
 }
