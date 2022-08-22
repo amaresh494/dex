2f8398dafb611f5fec327159b2d4156728b12355
amaresh494 <48903187+amaresh494@users.noreply.github.com>
2022-08-22 15:39:28 +0530

    Update templates.go

--- server/templates.go
+++ server/templates.go
@@ -7,6 +7,7 @@ import (
 	"io/fs"
 	"net/http"
 	"net/url"
+	"os"
 	"path"
 	"sort"
 	"strings"
@@ -89,6 +90,52 @@ func getFuncMap(c webConfig) (template.FuncMap, error) {
 //    |  |- (theme name)
 //    |- templates
 //
+type justFilesFilesystem struct {
+	fs http.FileSystem
+	// readDirBatchSize - configuration parameter for `Readdir` func
+	readDirBatchSize int
+}
+
+func (fs justFilesFilesystem) Open(name string) (http.File, error) {
+	f, err := fs.fs.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	return neuteredStatFile{File: f, readDirBatchSize: fs.readDirBatchSize}, nil
+}
+
+type neuteredStatFile struct {
+	http.File
+	readDirBatchSize int
+}
+
+func (e neuteredStatFile) Stat() (os.FileInfo, error) {
+	s, err := e.File.Stat()
+	if err != nil {
+		return nil, err
+	}
+	if s.IsDir() {
+	LOOP:
+		for {
+			fl, err := e.File.Readdir(e.readDirBatchSize)
+			switch err {
+			case io.EOF:
+				break LOOP
+			case nil:
+				for _, f := range fl {
+					if f.Name() == "index.html" {
+						return s, err
+					}
+				}
+			default:
+				return nil, err
+			}
+		}
+		return nil, os.ErrNotExist
+	}
+	return s, err
+}
+
 func loadWebConfig(c webConfig) (http.Handler, http.Handler, *templates, error) {
 	// fallback to the default theme if the legacy theme name is provided
 	if c.theme == "coreos" || c.theme == "tectonic" {
@@ -113,7 +160,7 @@ func loadWebConfig(c webConfig) (http.Handler, http.Handler, *templates, error) 
 		return nil, nil, nil, fmt.Errorf("read themes dir: %v", err)
 	}
 
-	static := http.FileServer(http.FS(staticFiles))
+	static := http.FileServer(justFilesFilesystem{fs: http.FS(staticFiles), readDirBatchSize: 2})
 	theme := http.FileServer(http.FS(themeFiles))
 
 	templates, err := loadTemplates(c, "templates")
